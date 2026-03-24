a497c2e9f8b7a470e6153eadc999feef86f47eec
Mohamed Tahar Atallah <120719194+mohamedtahar7@users.noreply.github.com>
2026-03-24 10:46:33 +0100

    Update offers.ts

--- lib/offers.ts
+++ lib/offers.ts
@@ -15,7 +15,7 @@ export const offers = [
   },
   {
     title: "E-Commerce Website Plan",
-    price: "42000.00",
+    price: "35000.00",
     whatfor:
       "Perfect for stores that want to sell online. With fast e-com websites tailored to your products and customers.",
     benefits: [
