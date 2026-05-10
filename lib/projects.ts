7cf3734a06b3c2216fafed039d76b7883aa62432
mohamedtahar7 <taharatallahx7@gmail.com>
2026-05-10 12:49:41 +0200

    removing seddik site

--- lib/projects.ts
+++ lib/projects.ts
@@ -39,13 +39,6 @@ export const projects = [
     category: "Landing Pages",
   },
   {
-    img: "https://i.postimg.cc/yd6rm7s6/aicraftedmedia.png",
-    name: "AiCraftedMedia 🤖🦾",
-    description: "Take your YouTube Channel to the next level with AI Editing.",
-    link: "https://aicraftedmedia.netlify.app/",
-    category: "Landing Pages",
-  },
-  {
     img: "https://i.postimg.cc/Px6VK05W/uniconfort.png",
     name: "Uniconfort 🪑🛒",
     description: "Get The Most Comfortable Furniture with Uniconfort.",
